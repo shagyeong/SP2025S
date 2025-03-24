01503f541896cd28417eb5aa42a1d9f2b7984ccc
jeeen0 <102155310+jeeen0@users.noreply.github.com>
2025-03-24 18:43:38 +0900

    Update urls.py

--- serverproject/backend/mainpage/urls.py
+++ serverproject/backend/mainpage/urls.py
@@ -1,7 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from .views import home
+from mainpage import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  # 대시보드 페이지
+    path('attendance/', views.attendance, name='attendance'),  # 출결 페이지
+    path('reservation/', views.reservation, name='reservation'),  # 장소 예약 페이지
 ]
-
