289ad21249ded46a0b463ff2e1e46f0e93af2722
jeeen0 <102155310+jeeen0@users.noreply.github.com>
2025-03-24 18:43:35 +0900

    Update views.py

--- serverproject/backend/mainpage/views.py
+++ serverproject/backend/mainpage/views.py
@@ -6,3 +6,14 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Hello, Django!")
 
+# 대시보드 페이지
+def index(request):
+    return render(request, 'index.html')
+
+# 출결 확인 페이지
+def attendance(request):
+    return render(request, 'attendance.html')
+
+# 장소 예약 페이지
+def reservation(request):
+    return render(request, 'reservation.html')
