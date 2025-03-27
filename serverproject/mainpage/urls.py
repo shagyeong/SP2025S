464fad4a0d5df52b0e6948a2e59560423882f994
jeeen0 <102155310+jeeen0@users.noreply.github.com>
2025-03-27 22:58:42 +0900

    메인페이지 URL 수정
    이제 http://127.0.0.1:8000/ 접속하면 메인페이지 로딩됨

--- serverproject/mainpage/urls.py
+++ serverproject/mainpage/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import student_view
+from . import views
+
+app_name = 'mainpage'
 
 urlpatterns = [
-    path('student/', student_view, name='student'),
+    path('', views.index, name='index'),  # 메인 대시보드
+    path('student/', views.student_view, name='student'),
+    path('documents/', views.documents_view, name='documents'),
+    path('notion/', views.notion_view, name='notion'),  # 필요 시
 ]
-
