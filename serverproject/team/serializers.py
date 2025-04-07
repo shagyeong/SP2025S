e45e134fdc7fc15f5c5a6a3fce87d99747a60d38
jeeen0 <102155310+jeeen0@users.noreply.github.com>
2025-04-07 18:40:15 +0900

    leader_name 추가

--- serverproject/team/serializers.py
+++ serverproject/team/serializers.py
@@ -3,8 +3,9 @@ from .models import Team
 
 class TeamSerializer(serializers.ModelSerializer):
     leader_id = serializers.CharField(source='leader.student_id', required=True)
+    leader_name = serializers.CharField(source='leader.name')
 
     class Meta:
         model = Team
-        fields = ['team_name', 'leader_id', 'mate1_id', 'mate2_id', 'mate3_id', 'mate4_id']
+        fields = ['team_name', 'leader_id', 'leader_name', 'mate1_id', 'mate2_id', 'mate3_id', 'mate4_id']
 
