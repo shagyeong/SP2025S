bdc459013752e29964e1b029c0ef0f5e088c06c6
jeeen0 <jinyoungbyun1125@gmail.com>
2025-06-02 20:20:03 +0900

    chat 비활성화

--- serverproject/chat/views.py
+++ serverproject/chat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def chat_view(request):
-    return render(request, 'chat/chat.html')
+    return HttpResponse("채팅 기능은 현재 비활성화되어 있습니다.")
